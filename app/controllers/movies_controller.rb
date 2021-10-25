b3d40e509a7d4c3e2c98331234beafecc6bb50db
Kiran Mohan Bodhapati Bodhapati <1004436@mymail.sutd.edu.sg>
2021-10-25 05:23:34 +0000

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show_hash = params[:ratings] || session[:ratings] || select_all_hash
+    @ratings_to_show = ratings_to_show
+    @movies = Movie.with_ratings(@ratings_to_show)
+    
+    if params[:sort].nil?
+      unless session[:sort].nil?
+        params[:sort] = session[:sort]
+      end
+    end      
+        
+    unless params[:sort].nil?
+      @movies = @movies.sort(params[:sort])
+      @sort = params[:sort]
+    end
+    
+    session[:sort] = @sort
+    session[:ratings] = @ratings_to_show_hash
+    
   end
 
   def new
@@ -43,5 +62,13 @@ class MoviesController < ApplicationController
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+  
+  def ratings_to_show
+    return @ratings_to_show_hash.keys
+  end
+  
+  def select_all_hash
+    return Hash[@all_ratings.map{|rating| [rating,"1"]}]
   end
 end
